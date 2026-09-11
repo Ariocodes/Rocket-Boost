415cb2ab4328408fae066e4faa396db19dc5a671
Ario <Ariobashiri@gmail.com>
2026-09-12 02:16:56 +0300

    FixedUpdate() and AddRelativeForce()

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -1,17 +1,30 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Controls;
 
 public class Movement : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [SerializeField]
+    InputAction thrust;
+    Rigidbody rb;
+    [SerializeField]
+    float thrustForce = 1000f; 
+
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
-    void Update()
+    void OnEnable()
     {
-        
+        thrust.Enable();
+    }
+
+    void FixedUpdate()
+    {
+        if (thrust.IsPressed())
+        {
+            rb.AddRelativeForce(Vector3.up * thrustForce * Time.fixedDeltaTime);
+        }
     }
 }
